cd529f0907eaaaea1ab199252beded740a5777c4
josephtjoshy <josephtjoshy9@gmail.com>
2021-06-22 19:07:07 +0530

    changes done

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:speech_to_text/speech_to_text.dart';
 import 'package:flutter_tts/flutter_tts.dart';
+
 void main() {
   runApp(MaterialApp(
     home: MyApp(),
   ));
 }
+
 class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({Key key}) : super(key: key);
 
   @override
   _MyAppState createState() => _MyAppState();
@@ -16,43 +18,51 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   SpeechToText speechToText = SpeechToText();
   FlutterTts flutterTts = FlutterTts();
-  String result = "";
+  String result = "", output = "Listening";
+
+  Future _listen() async {
+    await speechToText.initialize(
+      onStatus: (val) => print("LISTENING"),
+      onError: (val) => print("ERROR"),
+    );
+    await speechToText.listen(
+      onResult: (val) => setState(() {
+        result = val.recognizedWords;
+      }),
+    ).then((value) => speak());
+  }
+
+  Future speak() async {
+    if (result.toLowerCase() == 'hi') {
+      setState(() {
+        output = "Heloooo";
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: FloatingActionButton(
-        onPressed: (){
+        onPressed: () {
           setState(() {
             _listen();
           });
         },
       ),
-    body: SingleChildScrollView(
-      child: Text('$result'),
-    ),
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 200,
+              width: 200,
+            ),
+            Text("$result"),
+            Text("$output"),
+          ],
+        ),
+      ),
     );
   }
-  Future _listen()async{
-    await  speechToText.initialize(
-     onStatus: (val) => print("LISTENING"),
-     onError: (val) => print("ERROR"),
-   );
-    await speechToText.listen(
-     onResult: (val) => setState((){
-       result = val.recognizedWords;
-     }),
-   ).then((value) => speak());
-  }
-  Future speak()async{
-   if(result =="hi"){
-     setState(() {
-       print("setState");
-     });
-     String hi = "hi";
-     print("hello");
-     await flutterTts.speak('$hi').then((value) => null);
-   }
-  }
 }
-
